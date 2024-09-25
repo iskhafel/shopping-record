2d3c487049ac60bddf740ecc9f8f20c219aa75b3
faishalkamali <111752556+iskhafel@users.noreply.github.com>
2024-09-25 15:46:07 +0700

    convert html to jsx

---
+++ src/components/GroceryList.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+export default function GroceryList({ data }) {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              <input type="checkbox" />
+              <span>
+                {item.quantity} {item.name}
+              </span>
+              <button>&times;</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h1>Shopping Record 📝</h1>;
+}
